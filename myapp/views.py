41de79bab81605dcd9f8ebac49fc1add0d78e3db
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-19 15:14:06 +0900

    템플릿에서 for문으로 과일 리스트 출력

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
 def hello(request):
+    fruits = ['사과', '바나나', '딸기', '포도', '수박']
     context = {
-        'name': 'LWJ',
-        'message': '장고 템플릿 변수 넘기기 성공!'
+        'name': '우진',
+        'message': '과일 리스트입니다:',
+        'fruits': fruits
     }
     return render(request, 'myapp/hello.html', context)
