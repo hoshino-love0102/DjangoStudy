15ed2a9abf40c04ccb203cc1429ecb64caa9d544
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-19 23:43:17 +0900

    views db저장하기 수정

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Guest
 
 def hello_form(request):
     name = ''
@@ -7,16 +8,20 @@ def hello_form(request):
     if request.method == 'POST':
         name_input = request.POST.get('name', '').strip()
 
-        # 유효성 검사
         if not name_input:
             error = '이름을 입력해주세요.'
         elif name_input.lower() in ['admin', 'root', 'badword']:
             error = f'"{name_input}"은(는) 사용할 수 없는 이름입니다.'
         else:
-            name = name_input  # 유효할 때 출력
+            # DB에 저장
+            Guest.objects.create(name=name_input)
+            name = name_input
+
+    guests = Guest.objects.order_by('-created_at')
 
     context = {
         'name': name,
-        'error': error
+        'error': error,
+        'guests': guests
     }
     return render(request, 'myapp/hello_form.html', context)
