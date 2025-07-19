72b55800ef113df5c86a436ff30e1427216985df
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-19 16:03:48 +0900

    css적용

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 
 def hello_form(request):
-    name = None
+    name = ''
+    error = ''
 
     if request.method == 'POST':
-        name = request.POST.get('name')
+        name_input = request.POST.get('name', '').strip()
 
-    return render(request, 'myapp/hello_form.html', {'name': name})
+        # 유효성 검사
+        if not name_input:
+            error = '이름을 입력해주세요.'
+        elif name_input.lower() in ['admin', 'root', 'badword']:
+            error = f'"{name_input}"은(는) 사용할 수 없는 이름입니다.'
+        else:
+            name = name_input  # 유효할 때 출력
+
+    context = {
+        'name': name,
+        'error': error
+    }
+    return render(request, 'myapp/hello_form.html', context)
