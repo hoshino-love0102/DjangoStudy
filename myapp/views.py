99450a47be9f21ea97eb6506079a0fa0306a9794
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-19 15:18:58 +0900

    공통 템플릿 base.html추가

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 
 def hello(request):
-    fruits = ['사과', '바나나', '딸기', '포도', '수박']
+    fruits = ['사과', '바나나', '딸기']
     context = {
         'name': '우진',
-        'message': '과일 리스트입니다:',
         'fruits': fruits
     }
     return render(request, 'myapp/hello.html', context)
