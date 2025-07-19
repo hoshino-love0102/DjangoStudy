cfcef423fd9c90c97ec43e4d8c35dac370d27e14
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-19 15:11:26 +0900

    myapp/views.py 변수넘기기 수정

--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 def hello(request):
     context = {
         'name': 'LWJ',
-        'message': '장고 템플릿 변수 넘기기 성공'
+        'message': '장고 템플릿 변수 넘기기 성공!'
     }
     return render(request, 'myapp/hello.html', context)
