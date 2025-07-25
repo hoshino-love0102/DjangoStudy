5193d5966511f676b100b792817530a76f2859df
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-25 11:09:04 +0900

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import PhotoForm
 
-def hello_view(request):
-    return HttpResponse("Hello, Django!")
+def upload_view(request):
+    if request.method == 'POST':
+        form = PhotoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('upload')
+    else:
+        form = PhotoForm()
+    return render(request, 'myapp/upload.html', {'form': form})
