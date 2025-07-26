0b88034a955085d6053d446c10043e3aa140d0be
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-26 22:47:32 +0900

    Add show_photos view to display uploaded images

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect
 from .forms import PhotoForm
+from .models import Photo
 
 def upload_view(request):
     if request.method == 'POST':
         form = PhotoForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('upload')
+            return redirect('photo_list')
     else:
         form = PhotoForm()
     return render(request, 'myapp/upload.html', {'form': form})
+
+def show_photos(request):
+    photos = Photo.objects.order_by('-uploaded_at')
+    return render(request, 'myapp/photo_list.html', {'photos': photos})
