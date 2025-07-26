8ad4def67ba1db8902a41dafbd5b7f45cd1f3161
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-26 22:46:42 +0900

    Add URL pattern for photo_list view in myapp

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import upload_view
+from .views import upload_view, show_photos
 
 urlpatterns = [
     path('', upload_view, name='upload'),
+    path('photos/', show_photos, name='photo_list'),
 ]
