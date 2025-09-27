afb022ef3debc2cad1da2a348936d18fe18ba561
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-09-27 15:01:41 +0900

    feat(blog): add author field to Post model

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)  # 글 제목
-    content = models.TextField()  # 글 내용
-    created_at = models.DateTimeField(auto_now_add=True)  # 작성일
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="posts",
+    )
 
     def __str__(self):
         return self.title
