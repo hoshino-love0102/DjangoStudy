45e7c005f6cb17845f32d8a97b8b34ae81d70792
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-09-27 15:01:46 +0900

    feat(blog): implement post list, create and delete views with permissions

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 from .models import Post
 
+
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by("-created_at")
     return render(request, "blog/post_list.html", {"posts": posts})
+
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+
+        Post.objects.create(
+            title=title,
+            content=content,
+            created_at=timezone.now(),
+            author=request.user, # 작성자 저장
+        )
+        return redirect("post_list")
+
+    return render(request, "blog/post_new.html")
+
+
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    # 작성자 또는 관리자만 삭제 가능
+    if post.author != request.user and not request.user.is_superuser:
+        return HttpResponseForbidden("삭제 권한이 없습니다.")
+
+    if request.method == "POST":
+        post.delete()
+
+    return redirect("post_list")
