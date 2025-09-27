155e0ac3af96b419b317c0f67eaa5b0cf10b6ab3
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-09-27 15:02:23 +0900

    chore: register blog and accounts routes

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls")), # 블로그 앱 연결
+
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+
+    path("", include("blog.urls")),
 ]
