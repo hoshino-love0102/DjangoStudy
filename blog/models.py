9b767f9dd0bc2771e4f5cc247d81d82714c5b7eb
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-10-30 11:11:02 +0900

    feat(blog): add Comment model

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.post_id} - {self.author_id}"
