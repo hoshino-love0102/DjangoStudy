72013672008f9009c58a9a92919f1110457ef944
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-11-02 19:11:32 +0900

    feat(blog): add post detail and comment views

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,22 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
-from .models import Post
+from .models import Post, Comment
 
 
 def post_list(request):
     posts = Post.objects.all().order_by("-created_at")
     return render(request, "blog/post_list.html", {"posts": posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = post.comments.select_related("author").order_by("created_at")
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post, "comments": comments},
+    )
 
 
 @login_required
@@ -38,3 +48,33 @@ def post_delete(request, pk):
         post.delete()
 
     return redirect("post_list")
+
+
+@login_required
+def comment_create(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        content = request.POST.get("content", "").strip()
+        if content:
+            Comment.objects.create(
+                post=post,
+                author=request.user,
+                content=content,
+            )
+
+    return redirect("post_detail", pk=post.pk)
+
+
+@login_required
+def comment_delete(request, pk, comment_pk):
+    post = get_object_or_404(Post, pk=pk)
+    comment = get_object_or_404(Comment, pk=comment_pk, post=post)
+
+    if comment.author != request.user and not request.user.is_superuser:
+        return HttpResponseForbidden("삭제 권한이 없습니다.")
+
+    if request.method == "POST":
+        comment.delete()
+
+    return redirect("post_detail", pk=post.pk)
